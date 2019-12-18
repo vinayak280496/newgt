98997fc5b0585817e2a18693955b06014b9d5daa
Vinayak Kolaki <33669078+vinayak280496@users.noreply.github.com>
2019-12-18 11:42:57 +0530

    deleted

--- xamp/apache_stop.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /D %~dp0
-cmd.exe /C start "" /MIN call "C:\Users\Vinu\Desktop\Github\newgt\xamp\killprocess.bat" "httpd.exe"
-if not exist apache\logs\httpd.pid GOTO exit
-del apache\logs\httpd.pid
-
-:exit
--- xamp/cgi-bin/cgi.cgi
+++
@@ -1,5 +0,0 @@
-#!"C:\Users\Vinu\Desktop\Github\newgt\xamp\perl\bin\perl.exe"
-
-print "Content-Type: text/html\n\n";
-
-print "OK";
